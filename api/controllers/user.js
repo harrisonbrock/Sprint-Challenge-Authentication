8a7f75c7b79c5fb2892c12f41b04ba068247e7db
harrisonbrock <harrison@harrisonbrock.com>
2018-05-18 11:37:39 -0400

    Added errors for when username or password is missing

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -5,14 +5,14 @@ const createUser = (req, res) => {
   const { username, password } = req.body;
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
-  //   if (!username) {
-  //     res.status(404).send({error: 'You must include a username'});
-  //     return;
-  //   }
-  //   if(!password) {
-  //     res.status(404).send({error: 'Your must include a password'})
-  //       return;
-  //   }
+    if (!username) {
+      res.status(404).send({error: 'You must include a username'});
+      return;
+    }
+    if(!password) {
+      res.status(404).send({error: 'Your must include a password'})
+        return;
+    }
 
     const user = new User({username, password});
     console.log(user);
